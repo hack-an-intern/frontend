b3dc884bd7f5de14a7575c9419fd4f55b9c8c93e
Ved Prakash <vedprakash182001@gmail.com>
2022-10-29 02:13:45 +0530

    user

--- src/components/Buysell.js
+++ src/components/Buysell.js
@@ -8,7 +8,46 @@ const defaultProps = {};
  */
 const Buysell = () => {
     return <div>
-        buy and sell
+        <>
+        {/* Example single danger button */}
+        <div className="btn-group">
+            <button
+            type="button"
+            className="btn btn-danger dropdown-toggle"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            >
+            BUY
+            </button>
+            <ul className="dropdown-menu">
+            <li>
+                <a className="dropdown-item" href="#">
+                 Name
+                </a>
+            </li>
+            
+            </ul>
+        </div>
+        <div className="btn-group">
+            <button
+            type="button"
+            className="btn btn-danger dropdown-toggle"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            >
+            SELL
+            </button>
+            <ul className="dropdown-menu">
+            <li>
+                <a className="dropdown-item" href="#">
+                 Name
+                </a>
+            </li>
+            
+            </ul>
+        </div>
+        </>
+
     </div>;
 }
 
