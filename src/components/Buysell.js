91b9a0446f1f20e9bcc2f80d8d5722be09607ccf
Ved Prakash <vedprakash182001@gmail.com>
2022-10-29 01:06:33 +0530

    graph

---
+++ src/components/Buysell.js
@@ -0,0 +1,19 @@
+import React from 'react';
+const propTypes = {};
+
+const defaultProps = {};
+
+/**
+ * 
+ */
+const Buysell = () => {
+    return <div>
+        buy and sell
+    </div>;
+}
+
+Buysell.propTypes = propTypes;
+Buysell.defaultProps = defaultProps;
+// #endregion
+
+export default Buysell;
