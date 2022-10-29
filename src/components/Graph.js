ab31e2495cf3fa7aad2ea9b80755745fe85a4671
harshverma250202 <harshverma250202@gmail.com>
2022-10-29 21:54:43 +0530

    Co-authored-by: Harsh Wasnik <MUCCHU@users.noreply.github.com> Co-authored-by: vedprakash182001 <vedprakash182001@users.noreply.github.com>

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -32,19 +32,33 @@ const getconfig = (data) => {
 			},
 			chart: {
 				backgroundColor: '#ddddff',
+
 			},
 			series: getseries(data),
-			fillColor: {
-				linearGradient: {
-					x1: 0,
-					y1: 0,
-					x2: 0,
-					y2: 1
-				},
-				stops: [
-					[0, Highcharts.getOptions().colors[0]],
-					[1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-				]
+
+			rangeSelector: {
+				buttons: [{
+					type: 'hour',
+					count: 1,
+					text: '1h'
+				}, {
+					type: 'day',
+					count: 1,
+					text: '1d'
+				}, {
+					type: 'month',
+					count: 1,
+					text: '1m'
+				}, {
+					type: 'year',
+					count: 1,
+					text: '1y'
+				}, {
+					type: 'all',
+					text: 'All'
+				}],
+				inputEnabled: false, // it supports only days
+				selected: 4 // all
 			},
 		}
 	)
