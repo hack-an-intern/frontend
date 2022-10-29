ad680e7306c5dfd18d8e5c1dd7a3891b1ad60479
harshverma250202 <harshverma250202@gmail.com>
2022-10-29 20:16:40 +0530

    Buysell

--- src/components/Buysell.js
+++ src/components/Buysell.js
@@ -2,18 +2,26 @@ import { React, useState } from 'react';
 import { Col, Row, Input, Button } from 'antd';
 
 import { Tabs, Select, Radio, InputNumber } from 'antd';
-const { Option } = Select;
+import { useSelector, useDispatch } from 'react-redux';
 
+const { Option } = Select;
 const propTypes = {};
 
 // order type - Limit / market
 // trade-type - buy / sell
 const defaultProps = {};
 
+
+
 /**
  * 
  */
 const Buyui = () => {
+
+
+
+
+  const usersList = useSelector(state => state.user);
   const onChange = (value) => {
     console.log(`selected ${value}`);
   };
@@ -34,41 +42,47 @@ const Buyui = () => {
     console.log('submit');
   }
   return (<>
-    <Row style={{marginTop: '20px'}} align='middle'>
+    <Row style={{ marginTop: '20px' }} align='middle'>
       <Col className="gutter-row" span={10}>
-      <Select
-        showSearch
-        placeholder="Select the user"
-        optionFilterProp="children"
-        onChange={onChange}
-        onSearch={onSearch}
-        size={"large"}
-        style={{ width: '100%' }}
-        filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
-      >
-        <Option value="jack">Jack</Option>
+        <Select
+          showSearch
+          placeholder="Select the user"
+          optionFilterProp="children"
+          onChange={onChange}
+          onSearch={onSearch}
+          size={"large"}
+          style={{ width: '100%' }}
+          filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
+        >
+          {/* <Option value="jack">Jack</Option>
         <Option value="lucy">Lucy</Option>
-        <Option value="tom">Tom</Option>
-      </Select>
+        <Option value="tom">Tom</Option> */}
+          {
+            usersList.map((user, index) => { return <Option value={user.id} key={index}>{user.name}</Option> }
+            )
+          }
+
+
+        </Select>
       </Col>
       <Col className="gutter-row" offset={4} span={10}>
-      <Radio.Group
-        options={options}
-        onChange={onChangeOrdertype}
-        value={ordertype}
-        optionType="button"
-        buttonStyle="solid"
-      />
+        <Radio.Group
+          options={options}
+          onChange={onChangeOrdertype}
+          value={ordertype}
+          optionType="button"
+          buttonStyle="solid"
+        />
       </Col>
     </Row>
-    <Row style={{marginTop: '20px'}}>
+    <Row style={{ marginTop: '20px' }}>
       <Input size={"large"} placeholder="Number of stocks" />
     </Row>
-    {ordertype=="limit" && <Row style={{marginTop: '20px'}}>
+    {ordertype == "limit" && <Row style={{ marginTop: '20px' }}>
       <Input size={"large"} placeholder="Limit Value" />
     </Row>}
-    <Row style={{marginTop: '20px'}}>
-    <Button size={"large"} onClick={handleSubmit} style={{width: '100%'}} type="primary">Confirm Transaction</Button>
+    <Row style={{ marginTop: '20px' }}>
+      <Button size={"large"} onClick={handleSubmit} style={{ width: '100%' }} type="primary">Confirm Transaction</Button>
     </Row>
 
   </>
@@ -79,13 +93,13 @@ const Buysell = () => {
   const onChange1 = (value) => {
     console.log('changed', value);
   };
-  return <div style={{backgroundColor:"#363636", padding: '20px'}}>
+  return <div style={{ backgroundColor: "#363636", padding: '20px' }}>
     <>
-      <Tabs type="card"  defaultActiveKey="1">
-        <Tabs.TabPane style={{color: "#FFFFFF"}} tab="BUY" key="1" align="left">
+      <Tabs type="card" defaultActiveKey="1">
+        <Tabs.TabPane style={{ color: "#FFFFFF" }} tab="BUY" key="1" align="left">
           <Buyui />
         </Tabs.TabPane>
-        <Tabs.TabPane style={{color: "#FFFFFF"}} tab="SELL" key="2" align="right">
+        <Tabs.TabPane style={{ color: "#FFFFFF" }} tab="SELL" key="2" align="right">
           <Buyui />
         </Tabs.TabPane>
       </Tabs>
