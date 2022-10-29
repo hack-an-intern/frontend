d473605e4e4afb69946be6b88b7affb113571025
Harsh Wasnik <harshwasnik160602@gmail.com>
2022-10-29 13:32:31 +0530

    modified ui of buy and sell

--- src/components/Buysell.js
+++ src/components/Buysell.js
@@ -1,74 +1,92 @@
-import React from 'react';
-import { Tabs, Select,Radio,InputNumber } from 'antd';
+import { React, useState } from 'react';
+import { Col, Row, Input, Button } from 'antd';
+
+import { Tabs, Select, Radio, InputNumber } from 'antd';
 const { Option } = Select;
 
 const propTypes = {};
 
-
+// order type - Limit / market
+// trade-type - buy / sell
 const defaultProps = {};
 
 /**
  * 
  */
+const Buyui = () => {
+  const onChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+  const onSearch = (value) => {
+    console.log('search:', value);
+  };
+  const options = [
+    { label: 'Market', value: 'market' },
+    { label: 'Limit', value: 'limit' },
+  ];
+  const [ordertype, setOrdertype] = useState('market');
+  const onChangeOrdertype = ({ target: { value } }) => {
+    console.log('radio4 checked', value);
+    setOrdertype(value);
+  };
+  return (<>
+    <Row style={{marginTop: '20px'}} align='middle'>
+      <Col className="gutter-row" span={10}>
+      <Select
+        showSearch
+        placeholder="Select the user"
+        optionFilterProp="children"
+        onChange={onChange}
+        onSearch={onSearch}
+        size={"large"}
+        style={{ width: '100%' }}
+        filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
+      >
+        <Option value="jack">Jack</Option>
+        <Option value="lucy">Lucy</Option>
+        <Option value="tom">Tom</Option>
+      </Select>
+      </Col>
+      <Col className="gutter-row" offset={4} span={10}>
+      <Radio.Group
+        options={options}
+        onChange={onChangeOrdertype}
+        value={ordertype}
+        optionType="button"
+        buttonStyle="solid"
+      />
+      </Col>
+    </Row>
+    <Row style={{marginTop: '20px'}}>
+      <Input placeholder="Number of stocks" />
+    </Row>
+    {ordertype=="limit" && <Row style={{marginTop: '20px'}}>
+      <Input placeholder="Limit Value" />
+    </Row>}
+    <Row style={{marginTop: '20px'}}>
+    <Button style={{width: '100%'}} type="primary">Confirm Transaction</Button>
+    </Row>
 
-    const Buysell = () => {
-    const onChange = (value) => {
-        console.log(`selected ${value}`);
-        };
-        const onSearch = (value) => {
-        console.log('search:', value);
-        };
-        const onChange1 = (value) => {
-            console.log('changed', value);
-          };
-    return <div>
-        <>
-        <Tabs  defaultActiveKey="1">
+  </>
+  )
+}
+const Buysell = () => {
+
+  const onChange1 = (value) => {
+    console.log('changed', value);
+  };
+  return <div>
+    <>
+      <Tabs defaultActiveKey="1">
         <Tabs.TabPane tab="BUY" key="1" align="left">
-        <Select
-            showSearch
-            placeholder="Select the user"
-            optionFilterProp="children"
-            onChange={onChange}
-            onSearch={onSearch}
-            filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
-        >
-            <Option value="jack">Jack</Option>
-            <Option value="lucy">Lucy</Option>
-            <Option value="tom">Tom</Option>
-        </Select>
-
+          <Buyui />
         </Tabs.TabPane>
-        <Tabs.TabPane tab="SELL" key="2"align="right">
-              <Select
-            showSearch
-            placeholder="Select the user"
-            optionFilterProp="children"
-            onChange={onChange}
-            onSearch={onSearch}
-            filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
-        >
-            <Option value="jack">Jack</Option>
-            <Option value="lucy">Lucy</Option>
-            <Option value="tom">Tom</Option>
-        </Select>
+        <Tabs.TabPane tab="SELL" key="2" align="right">
+          <Buyui />
         </Tabs.TabPane>
-    </Tabs>
-        </>
-        <Radio.Group
-      defaultValue="c"
-      buttonStyle="solid"
-      style={{
-        marginTop: 16,
-        marginRight:20,
-      }}
-    >
-      <Radio.Button value="a">Market Order</Radio.Button>
-      <Radio.Button value="b">Limit Order</Radio.Button>
-    </Radio.Group>
-    <InputNumber min={1} max={10} defaultValue={3} onChange={onChange1} />
-    <button type="button" class="btn btn-success">Place Order</button>
-    </div>;
+      </Tabs>
+    </>
+  </div>;
 }
 
 Buysell.propTypes = propTypes;
